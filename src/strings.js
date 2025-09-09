f9d4c9d0adebc3f845159dc6d3f596773c25ebf5
Andre Velkov Janusev <velkovv97@gmail.com>
2025-09-09 10:05:25 +0200

    exercise complete

--- src/strings.js
+++ src/strings.js
@@ -8,21 +8,25 @@ const firstName = 'Jane'
 const secondName = 'Smith'
 
 // TODO: Replace null in the lines below using Javascript string operations
-//         and the variables above so that the tests pass.
+//       and the variables above so that the tests pass.
 //       eg  twoJanes = firstName + firstName
 //       NOT twoJanes = "JaneJane"
 
 // 1. Set this variable to firstName and secondName concatenated
-const fullName = null
+const fullName = firstName.concat(' ', secondName)
+console.log(fullName)
 
 // 2. Set this variable to the 10th character of the alphabet variable
-const tenthCharacterOfAlphabet = null
+const tenthCharacterOfAlphabet = alphabet.charAt(9)
+console.log(tenthCharacterOfAlphabet)
 
 // 3. Set this variable by calling a method on the alphabet variable to transform it to lower case
-const lowerCaseAlphabet = null
+const lowerCaseAlphabet = alphabet.toLowerCase()
+console.log(lowerCaseAlphabet)
 
 // 4. Set this variable by using a property on the alphabet variable to get it's length
-const numberOfLettersInAlphabet = null
+const numberOfLettersInAlphabet = alphabet.length
+console.log(numberOfLettersInAlphabet)
 
 // do not edit the exported object.
 module.exports = {
